95ab011517b40313b3e115dfffb9fc9d2364aad6
jiale qiu <jiale98chinoguay@gmail.com>
2023-09-17 16:13:30 -0400

    [Add]: connect server to mongoose db

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv").config(); // import and configure dotenv
+const mongoose = require("mongoose");
 
 const app = express(); // create our express backend server application
 const port = 4000;
+
+// Connect to our db;
+var db = mongoose.connect(
+  process.env.CONNECTION_STRING, 
+  { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("/", (req, res) => {
     res.send("Welcome to todo-backend server");
