b72eb61072d0c8320b80f5e5ff6c93b0f80a0f64
jiale qiu <jiale98chinoguay@gmail.com>
2023-09-16 22:38:37 -0400

    [Add]: Controllers

---
+++ todo-backend/controllers/todo.controller.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router(); // use express.Router() class to create our route handler system
+
+// GET todos
+
+// CREATE todo
+
+// DELETE todo
+
+// UPDATE todo
+
+module.exports = router; // export our router module system so other components can import this
---
+++ todo-backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express(); // create our express backend server application
+const port = 4000;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to todo-backend server");
+  });
+
+app.listen(port, (req, res) => {
+    console.log("Server is running on port: " + port);
+});
